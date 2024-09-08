3a3c48a32cb38aafd3885e7e922dfc8b629ed0cc
mohamadalabsi <absimoda1@gmail.com>
2024-09-08 14:29:45 +0200

    inserting Data into the DB

--- src/main/java/org/ME/Learning/Main.java
+++ src/main/java/org/ME/Learning/Main.java
@@ -4,7 +4,10 @@ package org.ME.Learning;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 
 import java.math.BigDecimal;
 
@@ -12,14 +15,17 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
         Car myCar = new Car();
-        myCar.setId(1001);
-        myCar.setModel("Kia");
-        myCar.setMake("Sephia");
-        BigDecimal price = new BigDecimal("8248.49");
+        myCar.setId(553);
+        myCar.setModel("Sonata");
+        myCar.setMake("hyundai");
+        BigDecimal price = new BigDecimal("12050.49");
         myCar.setPrice(price);
 
         Configuration  con = new Configuration().configure().addAnnotatedClass(Car.class  );
-        SessionFactory sf = con.buildSessionFactory();
+
+        ServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();  // just to get rid of the yello line under buildSessionFactory that is why it is a service
+
+        SessionFactory sf = con.buildSessionFactory(reg);
         Session session = sf.openSession();
 
         Transaction tx = session.beginTransaction();
